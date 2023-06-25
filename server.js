f6101a57a42dbc944e49512e460d4b4f7f20419a
Robson Coura <robson_coura@yahoo.com.br>
2023-06-25 16:22:12 -0300

    Configurando rotas

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const http = require("http")
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'Listagem de autores'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
     res.end('Curso de Node');
